31dd587e876ccf3936f7baad7020bdda5822c6e2
Fanouu <74674330+Fanouu@users.noreply.github.com>
2022-02-12 17:53:06 +0100

    /faction create and create faction système

--- src/Fanouu/FanFaction/Commands/subCommand/FCreateCommand.php
+++ src/Fanouu/FanFaction/Commands/subCommand/FCreateCommand.php
@@ -8,6 +8,7 @@ use Fanouu\FanFaction\Managers\FactionManager;
 use pocketmine\command\CommandSender;
 
 class FCreateCommand extends BaseSubCommand{
+    use traitCommand;
 
     protected function prepare(): void {
         $this->registerArgument(0, new RawStringArgument("faction name"));
@@ -17,9 +18,26 @@ class FCreateCommand extends BaseSubCommand{
     {
         $factionAPI = new FactionManager();
         if($factionAPI->asFaction($sender->getName())){
-            $sender->sendMessage("Tu a une fac");
+            $sender->sendMessage($this->messages("error_isInFaction"));
         }else{
-            $sender->sendMessage("Tu na pas de fac");
+            var_dump($this->config()->getNested("Factions.min-faction-name"));
+            if(strlen($args["faction name"]) <= (int)$this->config()->getNested("Factions.min-faction-name")){
+                $sender->sendMessage($this->messages("error_min_faction_name_lenght"));
+                return;
+            }
+
+            if(strlen($args["faction name"]) >= (int)$this->config()->getNested("Factions.max-faction-name")){
+                $sender->sendMessage($this->messages("error_max_faction_name_lenght"));
+                return;
+            }
+
+            if($factionAPI->existsFactionByName($args["faction name"])){
+                $sender->sendMessage(str_replace("{faction}", $args["faction name"], $this->messages("error_faction_exists")));
+                return;
+            }
+
+            $factionAPI->createFaction($sender->getName(), $args["faction name"]);
+            $sender->sendMessage(str_replace("{faction}", $args["faction name"], $this->messages("succes_faction_created")));
         }
     }
 
